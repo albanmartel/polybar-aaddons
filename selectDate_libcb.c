1e34964c7f91f1e1f7a00b8e07e74b02d365d4b3
Alban MARTEL <alban.f.j.martel@laposte.net>
2026-01-22 12:12:53 +0100

    Mise-à-jour du code pour permettre la compilation

--- selectDate_libcb.c
+++ selectDate_libcb.c
@@ -16,7 +16,7 @@ int main() {
         return 1;
     }
 
-    // 2. Appel de Zenity pour la sélection de la date [cite: 1]
+    // 2. Appel de Zenity
     fp = popen("zenity --calendar --text \"Sélectionner une date\" --date-format \"%Y-%m-%d\"", "r");
     if (fp == NULL) {
         perror("Erreur Zenity");
@@ -24,36 +24,31 @@ int main() {
         return 1;
     }
 
-    // Lecture du résultat [cite: 1]
     if (fgets(date, sizeof(date), fp) != NULL) {
-        date[strcspn(date, "\n")] = 0; // Nettoyage du saut de ligne
+        date[strcspn(date, "\n")] = 0;
     }
     pclose(fp);
 
-    // 3. Gestion du cas vide (Date courante si annulation) [cite: 1, 2]
+    // 3. Gestion du cas vide (Date courante)
     if (strlen(date) == 0) {
         time_t t = time(NULL);
         struct tm *tm_info = localtime(&t);
-        strftime(date, sizeof(date), "%Y-%m-%d", tm_info); [cite: 2]
+        strftime(date, sizeof(date), "%Y-%m-%d", tm_info);
 
         snprintf(info_cmd, sizeof(info_cmd), 
-                 "zenity --info --text \"Pas de sélection.\nLa date courante : %s\nest copiée dans le presse-papier\"", date); [cite: 2]
+                 "zenity --info --text \"Pas de sélection.\nLa date courante : %s\nest copiée dans le presse-papier\"", date);
     } else {
         snprintf(info_cmd, sizeof(info_cmd), 
-                 "zenity --info --text \"Date sélectionnée : %s\nest copiée dans le presse-papier\"", date); [cite: 1]
+                 "zenity --info --text \"Date sélectionnée : %s\nest copiée dans le presse-papier\"", date);
     }
 
-    // 4. Copie dans le presse-papier via libclipboard
-    // Cette fonction est universelle (X11/Wayland) et persistante
-    if (!clipboard_set_text(cb, date)) {
-        fprintf(stderr, "Échec de la copie dans le presse-papier\n");
-    }
+    // 4. Copie dans le presse-papier
+    clipboard_set_text(cb, date);
 
-    // 5. Affichage final et notification
-    printf("%s\n", date); [cite: 2]
+    // 5. Affichage et notification
+    printf("%s\n", date);
     system(info_cmd);
 
-    // Nettoyage
     clipboard_free(cb);
     return 0;
 }
